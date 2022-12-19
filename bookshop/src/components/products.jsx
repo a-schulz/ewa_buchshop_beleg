e805e1447fdd81a5062573ad1650d19dfeae2a06
Max Lange <max.lange@htw-dresden.de>
2022-12-19 14:38:22 +0100

    Frontend Produkte anzeigen

--- bookshop/src/components/products.jsx
+++ bookshop/src/components/products.jsx
@@ -1,8 +1,24 @@
 export const Products = () => {
 
+    var products = [];
+
+
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(response => response.json())
+      .then((usefulData) => {
+        products = usefulData;        
+      })
+      .catch((e) => {
+        console.error('An error occurred: ${e}')
+      });
     return(
         <div className="container">
-            Test
+            Products:
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>{product.title}</li>
+                ))}
+            </ul>
         </div>
 
     )

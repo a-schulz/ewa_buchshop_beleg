fee9559c7d57d2c1ead1ec20a7a0d52adc352061
a-schulz <schulz.alexander2002@gmail.com>
2022-12-19 14:52:15 +0100

    simple react with own api

--- bookshop/src/components/products.jsx
+++ bookshop/src/components/products.jsx
@@ -1,22 +1,25 @@
+import {useState} from "react";
+
 export const Products = () => {
 
-    var products = [];
+    var [products, setProducts] = useState([]);
 
 
-    fetch('https://jsonplaceholder.typicode.com/albums')
+    fetch('https://ivm108.informatik.htw-dresden.de/ewa/g14/php/index.php')
       .then(response => response.json())
       .then((usefulData) => {
-        products = usefulData;        
+        setProducts(usefulData);
       })
       .catch((e) => {
-        console.error('An error occurred: ${e}')
+        console.error(`An error occurred: ${e}`)
       });
+
     return(
         <div className="container">
             Products:
             <ul>
                 {products.map(product => (
-                    <li key={product.id}>{product.title}</li>
+                    <li key={product.ProduktID}>{product.Produkttitel}</li>
                 ))}
             </ul>
         </div>

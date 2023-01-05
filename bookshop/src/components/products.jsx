6ef038573fd0059f5420baaf3aaf66d61dda579e
Max Lange <s82072@htw-dresden.de>
2023-01-05 14:23:40 +0100

    Stripe Libraries installliert, Code Beispiel Liste

--- bookshop/src/components/products.jsx
+++ bookshop/src/components/products.jsx
@@ -1,9 +1,16 @@
 export const Products = () => {
-
+    const products = ['Ford', 'BMW', 'Audi'];
     return(
         <div className="container">
-            Test
+            <ul>
+                {Products.map((products) => <Products name={products} />)}
+            </ul>
+            Produkt: "Test Product" , Anzahl: Test Anzahl
+                <button class="btn btn-outline-success ml-3">+</button>
+				<button class="btn btn-outline-danger" >-</button>
+				<button class="btn btn-outline-primary" >Reset to 0</button>
         </div>
+        
 
     )
 }

756b4157665555d6ebc5012cbca1e79624b7226d
a-schulz <schulz.alexander2002@gmail.com>
2023-01-28 21:13:18 +0100

    added comments

--- php/index.php
+++ php/index.php
@@ -1,12 +1,13 @@
 <?php
-
 // Set up the database connection
 $username = 'g14';
 $password = 'gu67mim';
 $database = 'g14';
 
+// Create a new connection to the MySQL database
 $conn = new mysqli("localhost", $username, $password, $database)
-or die("Keine Verbindung möglich: " . mysql_error());
+    // if there is an error with the connection, stop the script and display the error
+    or die("Keine Verbindung möglich: " . mysql_error());
 
 // Set up the HTTP response headers
 // PHP should return JSON data
@@ -19,44 +20,61 @@ $httpMethod = $_SERVER["REQUEST_METHOD"];
 
 // Set up the HTTP response code and message
 $responseCode = 200;
-$responseMessage = "OK";
 http_response_code($responseCode);
 
 // Handle the request based on the HTTP method
 switch ($httpMethod) {
-    case "GET":
         // Handle GET requests
+    case "GET":
+        // Return a single item if an ID is provided
+        // Example: https://ivm108.informatik.htw-dresden.de/ewa/g14/php?id=1
         if (isset($_GET["id"])) {
-            // Return a single item if an ID is provided
             $id = $_GET["id"];
             if ($stmt = $conn->prepare("SELECT * FROM buecher WHERE produktid=?")) {
+                // Bind the parameter of type int (i) to the query
                 $stmt->bind_param("i", $id);
                 $stmt->execute();
             } else {
-                $error = $conn->errno . ' ' . $conn->error;
-                echo $error;
+                // Something went wrong
+                // Set the response code to 500 (Internal Server Error)
+                $responseCode = 500;
+                http_response_code($responseCode);
+                die(json_encode(["error" => mysqli_error($conn)]));
             }
             $result = $stmt->get_result();
             if (!$result) {
+                // Something went wrong
+                // Set the response code to 500 (Internal Server Error)
                 $responseCode = 500;
-                $responseMessage = "Internal Server Error";
-                $responseData = json_encode(["error" => mysqli_error($conn)]);
+                http_response_code($responseCode);
+                die(json_encode(["error" => mysqli_error($conn)]));
             }
+            // Return the result as JSON
             $rows = array();
-            while ($row = mysqli_fetch_assoc($result)) {
+            while ($row = $result->fetch_assoc()) {
                 $rows[] = $row;
             }
             $responseData = json_encode($rows);
+            mysqli_free_result($result);
+            mysqli_stmt_close($stmt);
         } else {
             // Return all items if no ID is provided
-            $stmt = ("SELECT * FROM buecher");
-            $result = mysqli_query($conn, $stmt);
+            $stmt = "SELECT * FROM buecher";
+            $result = $conn->query($stmt);
+            if (!$result) {
+                // Something went wrong
+                // Set the response code to 500 (Internal Server Error)
+                $responseCode = 500;
+                http_response_code($responseCode);
+                die(json_encode(["error" => mysqli_error($conn)]));
+            }
+            // Return the result as JSON
             $rows = array();
-            while ($row = mysqli_fetch_assoc($result)) {
+            // Fetch all rows as an associative array
+            while ($row = $result->fetch_assoc()) {
                 $rows[] = $row;
             }
             $responseData = json_encode($rows);
-//            echo $responseData;
         }
         break;
     default:
@@ -64,4 +82,24 @@ switch ($httpMethod) {
 }
 mysqli_close($conn);
 echo $responseData;
-?>
+
+/*
+// Example to bind prepared statements
+// Connect to the database
+$conn = new mysqli("localhost", "username", "password", "database");
+
+// Prepare the statement
+$stmt = $conn->prepare("INSERT INTO users (name, email) VALUES (?, ?)");
+
+// Bind the parameters
+$name = "John Doe";
+$email = "john.doe@example.com";
+$stmt->bind_param("ss", $name, $email);
+
+// Execute the statement
+$stmt->execute();
+
+// Close the statement and connection
+$stmt->close();
+$conn->close();
+*/
